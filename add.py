e8c14cf695c759c807a8f64da1039abf20212e15
shashankji89 <shashank761@gmail.com>
2026-07-22 18:58:28 +0530

    run_inden

--- add.py
+++ add.py
@@ -1,7 +1,6 @@
-num1=int(input("please give the number: "))
-num2=int(input("please give the number: "))
+num1 = int(input("please give the number: "))
+num2 = int(input("please give the number: "))
 def add_new(n, m):
-    return(n+m)
-    
-ans=add_new(num1,num2)
+    return (n+m)
+ans = add_new(num1, num2)
 print(ans)
