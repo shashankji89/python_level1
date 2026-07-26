65e6e1b712243d07f3187c883102fecacdc5c9f1
shashankji89 <shashank761@gmail.com>
2026-07-26 11:13:30 +0530

    new_change

--- giveing_num/n_n.py
+++ giveing_num/n_n.py
@@ -1,5 +1,9 @@
 num=int(input("please give the number: "))
 def num_new(num):
-    nums=num*num +1
-    print(nums)
+    if num>0:
+        print("its number is positive")
+    elif num==0:
+        print("number is zero")
+    else:
+        print("number is negative")        
 num_new(num)    
